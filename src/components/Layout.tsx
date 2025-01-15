0242afe93900b08172236e2a82d0484e059dd238
Guillermo Martin <gscalica@gmail.com>
2025-01-15 15:39:09 -0800

    Create 'LayoutProps' interface for 'children' prop in 'Layout' component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,13 @@
+import { ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-function Layout({ children }) {
+// "children" type:  https://stackoverflow.com/questions/55370851/how-to-fix-binding-element-children-implicitly-has-an-any-type-ts7031
+interface LayoutProps {
+  children: ReactNode;
+};
+
+function Layout({ children }: LayoutProps) {
   return (
     <div>
       <Navbar />
