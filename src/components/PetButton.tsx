cdf88e3e0606e24e32a49af19cfa681a2be8921d
Guillermo Martin <gscalica@gmail.com>
2025-01-20 15:14:12 -0800

    Create 'handleClick' function, add to 'PetButton' div

--- src/components/PetButton.tsx
+++ src/components/PetButton.tsx
@@ -1,16 +1,23 @@
+"use client";
 import Image from "next/image";
 
+// ---------- Interface ----------
 interface PetButtonProps {
   src: string;
   alt: string;
   text: string;
 };
 
+// ---------- Functions ----------
+const handleClick = () => {
+  alert("you clicked me!");
+};
+
 function PetButton({ src, alt, text }: PetButtonProps) {
   console.log(src, alt, text, "line 4");
 
   return (
-    <div>
+    <div onClick={handleClick}>
       <Image src={src} alt={alt} width={200} height={200}/>
       <p>{text}</p>
     </div>
