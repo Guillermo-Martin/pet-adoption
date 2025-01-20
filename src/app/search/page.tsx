1473229dda620097f79ee7c29148aa7e9ef1fa10
Guillermo Martin <gscalica@gmail.com>
2025-01-20 13:45:16 -0800

    Create 'search' folder for the 'search results' page (learning NextJS)

---
+++ src/app/search/page.tsx
@@ -0,0 +1,9 @@
+function SearchResults() {
+  return (
+    <div>
+      <h1>Search results page</h1>
+    </div>
+  );
+};
+
+export default SearchResults;
--- src/pages/search-results.tsx
+++
@@ -1,6 +0,0 @@
-function SearchResults() {
-  return <div>Search results page</div>
-};
-
-export default SearchResults;
-
