acf47c8024ebed4ce0cd5a7d0b63b6d9d0d3d33d
Guillermo Martin <gscalica@gmail.com>
2025-01-28 16:07:29 -0800

    Add types to 'PetContext' for 'createContext' and 'children' prop

--- src/context/PetContext.tsx
+++ src/context/PetContext.tsx
@@ -1,11 +1,34 @@
 "use client";
+import { createContext, ReactNode } from "react";
 
-import { createContext } from "react";
 
-const PetContext = createContext();
+interface ProviderProps {
+  children: ReactNode;
+};
+
+interface Animal {
+  id: number;
+  name: string;
+  gender: string;
+  age: string;
+  breeds: {
+    primary: string;
+  };
+  contact: {
+    address: {
+      city: string
+    }
+  };
+  primary_photo_cropped: {
+    full: string;
+  }
+};
+
+// create the context.  Tyep: The data is going to be an array of objects or null
+const PetContext = createContext<Animal[] | null>(null);
 
 // create the Provider
-function Provider({ children }) {
+function Provider({ children }: ProviderProps) {
   return (
     <PetContext.Provider value={5}>
       {children}
