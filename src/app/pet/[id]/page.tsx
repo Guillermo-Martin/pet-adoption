289f0997a858af565f6517d88ec650b400d5fe63
Guillermo Martin <gscalica@gmail.com>
2025-02-05 16:16:58 -0800

    Create 'PetDetails' interface, add loading logic to 'details' page

--- src/app/pet/[id]/page.tsx
+++ src/app/pet/[id]/page.tsx
@@ -1,8 +1,22 @@
 "use client";
-
+import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 
+
+interface PetDetails {
+  name: string;
+  age: string;
+  gender: string;
+  description: string;
+  size: string;
+  status: string;
+}
+
+
 function PetDetails() {
+  const [pet, setPet] = useState<PetDetails | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   // ---------- get the pet id from the URL ----------
   const params = useParams<{id: string}>();
 
@@ -15,31 +29,56 @@ function PetDetails() {
   const { id } = params;
   console.log("here's the pet's id", typeof id);
 
+
+  
+
+  
+
   // ---------- make request to get pet by id ----------
-  const fetchPetById = async (petId: string) => {
-    try {
-      // get the response
-      const response = await fetch(`/api/fetch-animals?id=${petId}`, { // <--- use query parameter
-        method: "GET"
-      });
+  useEffect(() => {
+    const fetchPetById = async (petId: string) => {
+      try {
+        // set isLoading to "true"
+        setIsLoading(true);
 
-      // convert the response to data
-      const data = await response.json();
-
-      console.log("individual pet data", data);
-
-      
-    } catch (error) {
-      console.log(error);
+        // get the response
+        const response = await fetch(`/api/fetch-animals?id=${petId}`, { // <--- use query parameter
+          method: "GET"
+        });
+  
+        // convert the response to data
+        const data = await response.json();
+  
+        // set the pet state
+        setPet(data);
+  
+        console.log("here is your pet data", data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
     };
-  };
 
-  fetchPetById(id);
+    fetchPetById(id);
+  }, [id]);
+  
+
+  
+
+
 
 
   return (
     <div>
-      <h1>Pet Details page</h1>
+      {(pet && isLoading === false) ? <h1>results</h1> : <h1>loading</h1>}
+      {/* <h1>Pet Details page</h1> */}
+      {/* <p>{pet.name}</p>
+      <p>{pet.age}</p>
+      <p>{pet.gender}</p>
+      <p>{pet.description}</p>
+      <p>{pet.size}</p>
+      <p>{pet.status}</p> */}
     </div>
   );
 };
