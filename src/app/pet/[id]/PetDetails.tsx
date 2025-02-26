dd9fc2fcd1c23dea8553c1a7362bafb0e44cac89
Guillermo Martin <gscalica@gmail.com>
2025-02-26 14:52:47 -0800

    Move Layout component in 'PetDetails' page

--- src/app/pet/[id]/PetDetails.tsx
+++ src/app/pet/[id]/PetDetails.tsx
@@ -167,71 +167,74 @@ function PetDetails() {
               // if there's pet data, isLoading is false, and "pet" isn't a number, show the data
               (pet && isLoading === false && typeof pet !== "number")
                 ?
-                  <div>
-                    {/* ---------- Image ---------- */}
-                    <div className="image-container size-[600px] mx-auto mb-[80px]">
-                      {/* Check to see if the data has an image available; if not, set image src to default image */}
-                      <Image src={hasPicture ? pet.animal.photos[0].full : "https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=3688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt={`Picture of ${pet.animal.name}`} width="600" height="600" className="size-full object-cover border-[6px] rounded-xl border-[#422206]" />
-                    </div>
-                    
-                    {/* ---------- Intro ---------- */}
-                    <div className="pet-intro text-[#422206] mb-[100px]">
-                      <h1 className="text-8xl font-bold border-b-[6px] border-b-[#422206] mb-[40px]">Hi!  I&apos;m {pet.animal.name}.</h1>
-                      <p className="text-[40px]">
-                        I&apos;m {pet.animal.age.toLowerCase() === "adult" ? "an" : "a"} <span className={pet.animal.gender === "Female" ? "text-[#fc7c86]" : "text-[#4369fc]" }>{pet.animal.age.toLowerCase()} {pet.animal.gender.toLowerCase()}</span> {pet.animal.breeds.primary.toLowerCase()}{pet.animal.breeds.secondary ? `, ${pet.animal.breeds.secondary.toLowerCase()} mix.` : "." }&nbsp;
-                        I&apos;m a <span className={pet.animal.size === "Small" ? "text-[#007b7f] text-3xl font-extralight" : pet.animal.size === "Medium" ? "text-[#d88c00]" : "text-[#d4194d] text-5xl font-extrabold"}>{pet.animal.size.toLowerCase()}</span> {pet.animal.species.toLowerCase()}.&nbsp;
-                        {pet.animal.tags.length !== 0 ? `Humans describe me as ${characteristics?.join(", ")}.` : null}
-                      </p>
-                      <p className="text-[40px]">I&apos;m <span className={pet.animal.status === "adoptable" ? "text-[#179E00]" : "text-[#000]"}>{pet.animal.status === "adoptable" ? "adoptable" : "taken"}</span>!</p>
-                    </div>
-                    
-                    {/* ---------- Description ---------- */}
-                    {/* <div className="pet-description text-[#422206] mb-[100px]">
-                      <h2 className="text-[64px] font-bold border-b-[6px] border-b-[#422206] mb-[40px]">Here&apos;s more information about me:</h2>
-                      <p className="text-2xl">{pet.animal.description}</p>
-                    </div> */}
-                    
-                    {/* ---------- Location ---------- */}
-                    <div className="pet-location text-[#422206] mb-[100px]">
-                      <h2 className="text-[64px] font-bold border-b-[6px] border-b-[#422206] mb-[40px]">You can find me here:</h2>
-                    
-                      {/* ----- Shelter container ----- */}
-                      <div className="shelter-container flex justify-content items-center">
-                        {/* ----- Shelter info ----- */}
-                        <div className="shelter-info w-[50%]">
-                          <h3 className="text-[40px]"><a href={pet.orgDetails.organization.website} target="_blank" rel="noopener noreferrer" className="underline hover:text-[#4369fc]">{pet.orgDetails.organization.website ? pet.orgDetails.organization.name : "Shelter not available"}</a></h3>
-                          <p className="text-[40px]">{pet.orgDetails.organization.address.address1}</p>
-                          <p className="text-[40px]">{pet.orgDetails.organization.address.city}, {pet.orgDetails.organization.address.state} {pet.orgDetails.organization.address.postcode}</p>
-                        </div>
+                  <Layout>
+                    <div>
+                      {/* ---------- Image ---------- */}
+                      <div className="image-container size-[600px] mx-auto mb-[80px]">
+                        {/* Check to see if the data has an image available; if not, set image src to default image */}
+                        <Image src={hasPicture ? pet.animal.photos[0].full : "https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=3688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt={`Picture of ${pet.animal.name}`} width="600" height="600" className="size-full object-cover border-[6px] rounded-xl border-[#422206]" />
+                      </div>
                       
-                        {/* ----------- MAP ---------- */}
-                        {/* Getting the leaflet map setup correctly: https://react-leaflet.js.org/docs/start-setup/ and https://github.com/PaulLeCam/react-leaflet/issues/1052 */}
-                        <MapContainer center={[parseFloat(pet.orgDetails.coordinates.lat), parseFloat(pet.orgDetails.coordinates.long)]} zoom={16} scrollWheelZoom={false} className="h-[400px] w-[50%]">
-                          <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                          />
-                          <Marker position={[parseFloat(pet.orgDetails.coordinates.lat), parseFloat(pet.orgDetails.coordinates.long)]} icon={ pet.animal.type === "Dog" ? customDogIcon : customCatIcon }>
-                            <Popup>
-                              {pet.orgDetails.organization.name}
-                              <br />
-                              {pet.orgDetails.organization.address.address1}
-                              <br />
-                              {pet.orgDetails.organization.address.city}, {pet.orgDetails.organization.address.state} {pet.orgDetails.organization.address.postcode}
-                            </Popup>
-                          </Marker>
-                        </MapContainer>
+                      {/* ---------- Intro ---------- */}
+                      <div className="pet-intro text-[#422206] mb-[100px]">
+                        <h1 className="text-8xl font-bold border-b-[6px] border-b-[#422206] mb-[40px]">Hi!  I&apos;m {pet.animal.name}.</h1>
+                        <p className="text-[40px]">
+                          I&apos;m {pet.animal.age.toLowerCase() === "adult" ? "an" : "a"} <span className={pet.animal.gender === "Female" ? "text-[#fc7c86]" : "text-[#4369fc]" }>{pet.animal.age.toLowerCase()} {pet.animal.gender.toLowerCase()}</span> {pet.animal.breeds.primary.toLowerCase()}{pet.animal.breeds.secondary ? `, ${pet.animal.breeds.secondary.toLowerCase()} mix.` : "." }&nbsp;
+                          I&apos;m a <span className={pet.animal.size === "Small" ? "text-[#007b7f] text-3xl font-extralight" : pet.animal.size === "Medium" ? "text-[#d88c00]" : "text-[#d4194d] text-5xl font-extrabold"}>{pet.animal.size.toLowerCase()}</span> {pet.animal.species.toLowerCase()}.&nbsp;
+                          {pet.animal.tags.length !== 0 ? `Humans describe me as ${characteristics?.join(", ")}.` : null}
+                        </p>
+                        <p className="text-[40px]">I&apos;m <span className={pet.animal.status === "adoptable" ? "text-[#179E00]" : "text-[#000]"}>{pet.animal.status === "adoptable" ? "adoptable" : "taken"}</span>!</p>
+                      </div>
+                      
+                      {/* ---------- Description ---------- */}
+                      {/* <div className="pet-description text-[#422206] mb-[100px]">
+                        <h2 className="text-[64px] font-bold border-b-[6px] border-b-[#422206] mb-[40px]">Here&apos;s more information about me:</h2>
+                        <p className="text-2xl">{pet.animal.description}</p>
+                      </div> */}
+                      
+                      {/* ---------- Location ---------- */}
+                      <div className="pet-location text-[#422206] mb-[100px]">
+                        <h2 className="text-[64px] font-bold border-b-[6px] border-b-[#422206] mb-[40px]">You can find me here:</h2>
+                      
+                        {/* ----- Shelter container ----- */}
+                        <div className="shelter-container flex justify-content items-center">
+                          {/* ----- Shelter info ----- */}
+                          <div className="shelter-info w-[50%]">
+                            <h3 className="text-[40px]"><a href={pet.orgDetails.organization.website} target="_blank" rel="noopener noreferrer" className="underline hover:text-[#4369fc]">{pet.orgDetails.organization.website ? pet.orgDetails.organization.name : "Shelter not available"}</a></h3>
+                            <p className="text-[40px]">{pet.orgDetails.organization.address.address1}</p>
+                            <p className="text-[40px]">{pet.orgDetails.organization.address.city}, {pet.orgDetails.organization.address.state} {pet.orgDetails.organization.address.postcode}</p>
+                          </div>
+                        
+                          {/* ----------- MAP ---------- */}
+                          {/* Getting the leaflet map setup correctly: https://react-leaflet.js.org/docs/start-setup/ and https://github.com/PaulLeCam/react-leaflet/issues/1052 */}
+                          <MapContainer center={[parseFloat(pet.orgDetails.coordinates.lat), parseFloat(pet.orgDetails.coordinates.long)]} zoom={16} scrollWheelZoom={false} className="h-[400px] w-[50%]">
+                            <TileLayer
+                              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={[parseFloat(pet.orgDetails.coordinates.lat), parseFloat(pet.orgDetails.coordinates.long)]} icon={ pet.animal.type === "Dog" ? customDogIcon : customCatIcon }>
+                              <Popup>
+                                {pet.orgDetails.organization.name}
+                                <br />
+                                {pet.orgDetails.organization.address.address1}
+                                <br />
+                                {pet.orgDetails.organization.address.city}, {pet.orgDetails.organization.address.state} {pet.orgDetails.organization.address.postcode}
+                              </Popup>
+                            </Marker>
+                          </MapContainer>
+                        </div>
+                      </div>
+                      
+                      {/* ---------- Contact ---------- */}
+                      <div className="pet-contact text-[#422206] mb-[100px]">
+                        <h2 className="text-[64px] font-bold border-b-[6px] border-b-[#422206] mb-[40px]">Interested?  Reach out using the information below!</h2>
+                        <p className="text-2xl"><span className="font-bold">Email:</span> <a href={`mailto:${pet.orgDetails.organization.email}`} className="underline hover:text-[#4369fc]">{pet.orgDetails.organization.email ? pet.orgDetails.organization.email : "Not available"}</a></p>
+                        <p className="text-2xl"><span className="font-bold">Phone:</span> {pet.orgDetails.organization.phone ? pet.orgDetails.organization.phone : "Not available"}</p>
+                        <p className="text-2xl"><span className="font-bold">Website:</span> <a href={pet.orgDetails.organization.website} target="_blank" rel="noopener noreferrer" className="underline hover:text-[#4369fc]">{pet.orgDetails.organization.website ? pet.orgDetails.organization.website : "Not available"}</a></p>
                       </div>
                     </div>
-                    
-                    {/* ---------- Contact ---------- */}
-                    <div className="pet-contact text-[#422206] mb-[100px]">
-                      <h2 className="text-[64px] font-bold border-b-[6px] border-b-[#422206] mb-[40px]">Interested?  Reach out using the information below!</h2>
-                      <p className="text-2xl"><span className="font-bold">Email:</span> <a href={`mailto:${pet.orgDetails.organization.email}`} className="underline hover:text-[#4369fc]">{pet.orgDetails.organization.email ? pet.orgDetails.organization.email : "Not available"}</a></p>
-                      <p className="text-2xl"><span className="font-bold">Phone:</span> {pet.orgDetails.organization.phone ? pet.orgDetails.organization.phone : "Not available"}</p>
-                      <p className="text-2xl"><span className="font-bold">Website:</span> <a href={pet.orgDetails.organization.website} target="_blank" rel="noopener noreferrer" className="underline hover:text-[#4369fc]">{pet.orgDetails.organization.website ? pet.orgDetails.organization.website : "Not available"}</a></p>
-                    </div>
-                  </div>
+                  </Layout>
+                  
                 :
                   // otherwise, show the loading status
                   // <h1>Loading...</h1>
