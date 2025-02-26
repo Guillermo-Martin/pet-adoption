a79e9bfd17b4cad02dc45d39cc99a00146c0fd92
Guillermo Martin <gscalica@gmail.com>
2025-02-26 15:34:32 -0800

    Add basic 'Search' functionality in 'Navbar' component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,24 @@
+import{ useState } from "react";
 import Link from "next/link";
 
+
 function Navbar() {
+  // ---------- State ----------
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
+  // ---------- Functions ----------
+  const handleClick = () => {
+    // set isSearchActive
+    setIsSearchActive(!isSearchActive);
+  };
+
+  // ---------- Component ----------
   return (
     <div>
       <Link href="/"><p>Home</p></Link>
-      <p>Search</p>
+      <p onClick={handleClick}>Search again</p>
+      {/* if "isSearchActive" is true, show the search box, otherwise hide it */}
+      {isSearchActive && "showing search box"}
     </div>
   );
 };
