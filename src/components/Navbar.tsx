0f010e3cb7cbe5a9929693967aebc06635c93ac1
Guillermo Martin <gscalica@gmail.com>
2025-02-26 16:22:24 -0800

    Add pet selection functionality to navbar 'Search' dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,27 +6,32 @@ import PetButton from "@/components/PetButton";
 function Navbar() {
   // ---------- State ----------
   const [isSearchActive, setIsSearchActive] = useState(false);
+  const [isSelected, setIsSelected] = useState("");
 
   // ---------- Functions ----------
-  const handleClick = () => {
+  // open/close search dropdown
+  const handleSearchClick = () => {
     // set isSearchActive
     setIsSearchActive(!isSearchActive);
+  };
+
+  // animal selection
+  const handleClick = (animal: string) => {
+    setIsSelected(animal);
   };
 
   // ---------- Component ----------
   return (
     <div>
       <Link href="/"><p>Home</p></Link>
-      <p onClick={handleClick}>Search again</p>
+      <p onClick={handleSearchClick}>Search again</p>
       {/* if "isSearchActive" is true, show the search box, otherwise hide it */}
       {
         isSearchActive && 
         <div className="search-dropdown">
           <p>What are you looking for?</p>
-          {/* <PetButton src="/images/dog-icon.png" alt="Dog" text="Dog" onClick={handleClick} animalType="dog" isSelected={isSelected === "dog"} /> */}
-          <PetButton src="/images/dog-icon.png" alt="Dog" text="Dog" onClick={handleClick} animalType="dog" />
-          {/* <PetButton src="/images/cat-icon.png" alt="Cat" text="Cat" onClick={handleClick} animalType="cat" isSelected={isSelected === "cat"} /> */}
-          <PetButton src="/images/cat-icon.png" alt="Cat" text="Cat" onClick={handleClick} animalType="cat" />
+          <PetButton src="/images/dog-icon.png" alt="Dog" text="Dog" onClick={handleClick} animalType="dog" isSelected={isSelected === "dog"} />
+          <PetButton src="/images/cat-icon.png" alt="Cat" text="Cat" onClick={handleClick} animalType="cat" isSelected={isSelected === "cat"} />
         </div>
       }
     </div>
