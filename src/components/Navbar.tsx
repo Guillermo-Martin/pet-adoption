80440147a3e7c0e9a51f9692bbf6cf90ad8343dd
Guillermo Martin <gscalica@gmail.com>
2025-02-26 17:13:03 -0800

    Add basic styling and positioning for Navbar 'Search' dropdown using Tailwind

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -71,28 +71,27 @@ function Navbar() {
 
   // ---------- Component ----------
   return (
-    <div>
+    <div className="navbar relative bg-[red]">
       <Link href="/"><p>Home</p></Link>
       <p onClick={handleSearchClick}>Search again</p>
       {/* if "isSearchActive" is true, show the search box, otherwise hide it */}
       {
         isSearchActive && 
-        <div className="search-dropdown">
+        <div className="search-dropdown absolute w-full bg-[blue]">
           <p>What are you looking for?</p>
-          <PetButton src="/images/dog-icon.png" alt="Dog" text="Dog" onClick={handleClick} animalType="dog" isSelected={isSelected === "dog"} />
-          <PetButton src="/images/cat-icon.png" alt="Cat" text="Cat" onClick={handleClick} animalType="cat" isSelected={isSelected === "cat"} />
+          <div className="form-container flex justify-center items-center">
+            <PetButton src="/images/dog-icon.png" alt="Dog" text="Dog" onClick={handleClick} animalType="dog" isSelected={isSelected === "dog"} />
+            <PetButton src="/images/cat-icon.png" alt="Cat" text="Cat" onClick={handleClick} animalType="cat" isSelected={isSelected === "cat"} />
 
-          {/* ---------- Zipcode ---------- */}
-          <form onSubmit={handleSubmit} className="flex items-center justify-center flex-col">
-          {/* <form className="flex items-center justify-center flex-col"> */}
-                <label className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4">Enter your zipcode</label>
-
-                
-                  <input name="zipcode" type="text" pattern="[0-9]{5}" title="Five digit zip code" onChange={handleChange} value={zipcode} className="border-2 border-[#422206] rounded-xl h-[40px] pl-[8px] pt-[6px] text-2xl w-[40%] lg:w-[24%] mb-4" />
-                  <button className="text-base sm:text-2xl border-4 border-[#422206] rounded-3xl w-[40%] lg:w-[24%] bg-[#ffda9c] flex items-center justify-center active:scale-95">Submit</button>
-                
-                
-              </form>
+            {/* ---------- Zipcode ---------- */}
+            <form onSubmit={handleSubmit} className="flex items-center justify-center flex-col">
+            {/* <form className="flex items-center justify-center flex-col"> */}
+              <label className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4">Enter your zipcode</label>
+                <input name="zipcode" type="text" pattern="[0-9]{5}" title="Five digit zip code" onChange={handleChange} value={zipcode} className="border-2 border-[#422206] rounded-xl h-[40px] pl-[8px] pt-[6px] text-2xl w-[40%] lg:w-[24%] mb-4" />
+                <button className="text-base sm:text-2xl border-4 border-[#422206] rounded-3xl w-[40%] lg:w-[24%] bg-[#ffda9c] flex items-center justify-center active:scale-95">Submit</button>
+            </form>
+          </div>
+          
         </div>
       }
       {isLoading && "loading..."}
