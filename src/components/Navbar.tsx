64df6c6f8507671414a603d914583dabf8ae9ca5
Guillermo Martin <gscalica@gmail.com>
2025-02-26 16:35:33 -0800

    Add form to Navbar 'Search' dropdown and get input to work

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import{ useState } from "react";
+import{ useState, FormEvent, ChangeEvent } from "react";
 import Link from "next/link";
 import PetButton from "@/components/PetButton";
 
@@ -7,6 +7,7 @@ function Navbar() {
   // ---------- State ----------
   const [isSearchActive, setIsSearchActive] = useState(false);
   const [isSelected, setIsSelected] = useState("");
+  const [zipcode, setZipcode] = useState("");
 
   // ---------- Functions ----------
   // open/close search dropdown
@@ -20,6 +21,12 @@ function Navbar() {
     setIsSelected(animal);
   };
 
+  // zipcode input
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setZipcode(event.target.value);
+  };
+
+
   // ---------- Component ----------
   return (
     <div>
@@ -32,6 +39,18 @@ function Navbar() {
           <p>What are you looking for?</p>
           <PetButton src="/images/dog-icon.png" alt="Dog" text="Dog" onClick={handleClick} animalType="dog" isSelected={isSelected === "dog"} />
           <PetButton src="/images/cat-icon.png" alt="Cat" text="Cat" onClick={handleClick} animalType="cat" isSelected={isSelected === "cat"} />
+
+          {/* ---------- Zipcode ---------- */}
+          {/* <form onSubmit={handleSubmit} className="flex items-center justify-center flex-col"> */}
+          <form className="flex items-center justify-center flex-col">
+                <label className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4">Enter your zipcode</label>
+
+                
+                  <input name="zipcode" type="text" pattern="[0-9]{5}" title="Five digit zip code" onChange={handleChange} value={zipcode} className="border-2 border-[#422206] rounded-xl h-[40px] pl-[8px] pt-[6px] text-2xl w-[40%] lg:w-[24%] mb-4" />
+                  <button className="text-base sm:text-2xl border-4 border-[#422206] rounded-3xl w-[40%] lg:w-[24%] bg-[#ffda9c] flex items-center justify-center active:scale-95">Submit</button>
+                
+                
+              </form>
         </div>
       }
     </div>
