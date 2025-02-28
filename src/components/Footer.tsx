7183610104e3fcc552225043a91546062a6abb1a
Guillermo Martin <gscalica@gmail.com>
2025-02-27 16:29:12 -0800

    Add content to Footer component and style using Tailwind

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
-    <div>
-      <h2>Footer</h2>
+    <div className="footer border-t-[6px] border-t-[#422206] px-4 py-3">
+      <p>Made using Next.js, Typescript, React, and TailwindCSS.  Designed in Figma.
+        Icons by <a href="https://www.freepik.com/author/aomam-ss/icons" target="_blank" rel="noopener noreferrer" className="underline hover:text-[#4369fc]">AomAm.</a> © Copyright 2025.
+      </p>
     </div>
   );
 };
