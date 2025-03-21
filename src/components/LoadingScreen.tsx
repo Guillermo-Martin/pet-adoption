2c3aff7bfe24ad39c3fec1eeeec6790ce99758a7
Guillermo Martin <gscalica@gmail.com>
2025-03-21 16:27:06 -0700

    Add bounce animation to loading screen image using Tailwind

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -17,7 +17,7 @@ interface LoadingScreenProps {
 function LoadingScreen({ message, imageSrc, alt, error }: LoadingScreenProps) {
   return (
     <div className="loading-container-content">
-      <Image src={imageSrc} alt={alt} width={200} height={200} className="w-[40%] xs:w-[60%] lg:w-[80%] max-w-[102.4px] xs:max-w-[163.54px] lg:max-w-[218.06px] mx-auto mb-6" />
+      <Image src={imageSrc} alt={alt} width={200} height={200} className="w-[40%] xs:w-[60%] lg:w-[80%] max-w-[102.4px] xs:max-w-[163.54px] lg:max-w-[218.06px] mx-auto mb-6 animate-bounce" />
       <p className="text-center text-lg xs:text-xl">{message}</p>
       {/* If "error" is true, show a link to the homepage */}
       {error && <Link href="/" className="block mx-auto mt-2 w-fit underline hover:text-[#4369fc] transition duration-150">Go back to the homepage.</Link>}
